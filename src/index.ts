7b45d57579981052448487a0ea150f04f7276cee
Emmanuel Nwankwo <cnwankwom@gmail.com>
2024-10-23 01:01:11 +0100

    feat: add NestJs config compatibility

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,58 @@ import path from 'path';
 import dotenv from 'dotenv';
 import type { ValidatedEnv, EnvSchema, EnvSchemaItem } from './types';
 
+
+const loadEnvFile = (envFile: string = '.env'): NodeJS.ProcessEnv => {
+    let env: NodeJS.ProcessEnv = process.env;
+    const envPath = path.resolve(process.cwd(), envFile);
+
+    if (fs.existsSync(envPath)) {
+        const result = dotenv.config({ path: envPath });
+        if (result.error) {
+            console.error(`Error loading ${envFile}: ${result.error.message}`);
+        } else {
+            env = { ...process.env, ...result.parsed };
+        }
+    }
+
+    return env;
+};
+
+const validateEnvVariable = <T>(key: string, envValue: string | undefined, schemaItem: EnvSchemaItem<T>, errors: string[]): T | undefined => {
+    if (envValue === undefined) {
+        if (schemaItem.required) {
+            errors.push(`Missing required field: ${key}`);
+        } else if ('default' in schemaItem) {
+            return schemaItem.default;
+        }
+        return;
+    }
+
+    switch (schemaItem.type) {
+        case Number:
+            {
+                const num = Number(envValue);
+                if (isNaN(num)) {
+                    errors.push(`${key} should be a number`);
+                } else {
+                    return num as T;
+                }
+                break;
+            }
+        case Boolean:
+            if (envValue !== 'true' && envValue !== 'false') {
+                errors.push(`${key} should be a boolean`);
+            } else {
+                return (envValue === 'true') as T;
+            }
+            break;
+        case String:
+            return envValue as T;
+        default:
+            errors.push(`${key} has an unsupported type`);
+    }
+};
+
 /**
  * Validates and loads environment variables based on a provided schema.
  * 
@@ -22,68 +74,57 @@ import type { ValidatedEnv, EnvSchema, EnvSchemaItem } from './types';
  * });
  * 
  */
-const validateEnv = <T extends EnvSchema>(schema: T, envFile?: string): ValidatedEnv<T> => {
-    const fileToLoad = envFile || '.env';
-    let env: NodeJS.ProcessEnv = process.env;
+function validateEnv<T extends EnvSchema>(schema: T, envFile?: string): ValidatedEnv<T>;
+/**
+ * Validates and loads environment variables based on a provided schema.
+ * 
+ * @template T - The type of the environment schema.
+ * @param {T} schema - An object describing the expected environment variables and their types.
+ *                     Each key in the schema can be either a constructor (String, Number, Boolean)
+ *                     or an object with `type`, `required`, and `default` properties.
+ * @param {Record<string, any>} [config] - A configuration object containing environment variables for NestJS
+ * @param {string} [envFile] - The name of the environment file to load. Defaults to '.env' if not provided.
+ * 
+ * @throws {Error} Throws an error if required environment variables are missing or if any validation fails.
+ * 
+ * @example
+ * ConfigModule.forRoot({
+ *   isGlobal: true,
+ *   validate: (config) => validateEnv(envSchema, config),
+ * }),
+ * 
+ */
+function validateEnv<T extends EnvSchema>(schema: T, config: Record<string, any>, envFile?: string): ValidatedEnv<T>;
 
-    const envPath = path.resolve(process.cwd(), fileToLoad); // Resolve the path of the environment file
-
-    if (fs.existsSync(envPath)) {
-        const result = dotenv.config({ path: envPath });
-        if (result.error) {
-            console.error(`Error loading ${fileToLoad}: ${result.error.message}`);
-        } else {
-            env = { ...process.env, ...result.parsed }; // Merge existing process.env with loaded env variables
-        }
-    }
-
+function validateEnv<T extends EnvSchema>(schema: T, configOrEnvFile?: Record<string, any> | string, envFile?: string): ValidatedEnv<T> {
+    envFile = (typeof configOrEnvFile === 'string') ? configOrEnvFile : envFile;
+    const env: NodeJS.ProcessEnv = loadEnvFile(envFile);
     const envConfig = {} as ValidatedEnv<T>;
     const errors: string[] = [];
 
     for (const [key, schemaValue] of Object.entries(schema)) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const schemaItem: EnvSchemaItem<any> = typeof schemaValue === 'function'
             ? { type: schemaValue, required: true }
             : { ...schemaValue, required: schemaValue.required !== false };
 
         const envValue = env[key];
 
-        if (envValue === undefined) {
-            if (schemaItem.required) {
-                errors.push(`Missing required field: ${key}`);
-            } else if ('default' in schemaItem) {
-                envConfig[key as keyof T] = schemaItem.default;
-            }
-            continue;
-        }
-
-        switch (schemaItem.type) {
-            case Number:
-                const num = Number(envValue);
-                if (isNaN(num)) {
-                    errors.push(`${key} should be a number`);
-                } else {
-                    envConfig[key as keyof T] = num as any;
-                }
-                break;
-            case Boolean:
-                if (envValue !== 'true' && envValue !== 'false') {
-                    errors.push(`${key} should be a boolean`);
-                } else {
-                    envConfig[key as keyof T] = (envValue === 'true') as any;
-                }
-                break;
-            case String:
-                envConfig[key as keyof T] = envValue as any;
-                break;
-            default:
-                errors.push(`${key} has an unsupported type`);
+        const validatedValue = validateEnvVariable(key, envValue, schemaItem, errors);
+        if (validatedValue !== undefined) {
+            envConfig[key as keyof T] = validatedValue;
         }
     }
 
     if (errors.length) {
-        console.error('Environment validation failed:');
-        errors.forEach(error => { console.error(`- ${error}`); });
-        process.exit(1);
+        if (typeof configOrEnvFile === 'object') {
+            const formattedErrors = errors.map(error => `- ${error}`).join('\n');
+            throw new Error(`Environment validation failed:\n${formattedErrors}`);
+        } else {
+            console.error('Environment validation failed:');
+            errors.forEach(error => { console.error(`- ${error}`); });
+            process.exit(1);
+        }
     }
 
     return envConfig;
